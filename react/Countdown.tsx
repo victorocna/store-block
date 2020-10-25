44aeed53e7c3dc5d5eb4245a42749191f00bb29d
victorocna <victorocnarescu@gmail.com>
2020-10-25 21:15:13 +0200

    add countdown component

--- react/Countdown.tsx
+++ react/Countdown.tsx
@@ -1,16 +1,29 @@
 import React from 'react'
 
-interface CountdownProps { }
+interface CountdownProps {
+  targetDate: string
+}
 
-const Countdown: StorefrontFunctionComponent<CountdownProps> = ({ }) => {
-  return <div></div>
+const Countdown: StorefrontFunctionComponent<CountdownProps> = ({ targetDate }) => {
+  return (
+    <div>
+      <h1>{ targetDate }</h1>
+    </div>
+  )
 }
 
 Countdown.schema = {
   title: 'editor.countdown.title',
   description: 'editor.countdown.description',
   type: 'object',
-  properties: {},
+  properties: {
+    targetDate: {
+      title: 'Final date',
+      description: 'Final date used in the countdown',
+      type: 'string',
+      default: null,
+    },
+  },
 }
 
 export default Countdown
